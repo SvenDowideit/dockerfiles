e85262a103747d69e1372fd7d92f669ba28620e9
Sven Dowideit <SvenDowideit@fosiki.com>
2013-11-05 20:06:18 +1000

    what I thought I had running on the other server, but of course, it doesn't work here :(

--- debian-dnsmasq/run_dhcp.sh
+++ debian-dnsmasq/run_dhcp.sh
@@ -13,9 +13,27 @@ ip link add dhcp_server link eth5 type macvlan mode bridge
 #or dynamic
 dhclient dhcp_server
 
+export EXTERNAL_DHCP_SERVER_IP=$(ip add show dev dhcp_server | grep 'inet ' | sed 's/.*inet //' | sed 's/\/.*//')
+
 #create the iptables chain
-iptables -t nat -N dchpserver
+iptables -t nat -N dhcpserver
 
-docker run -d -name=dnsmasq svendowideit/dnsmasq dnsmasq -d
+/home/sven/src/docker/un/docker-0.6.5-dev run -i -t  -d -name=dnsmasq svendowideit/dnsmasq /bin/bash 
+#dnsmasq -d
+
+export DHCP_SERVER_IP=$(/home/sven/src/docker/un/docker-0.6.5-dev inspect dnsmasq | grep IPAddress | sed 's/",//' | sed 's/.*"//')
 
 
+iptables -t nat -A PREROUTING -p all -d $EXTERNAL_DHCP_SERVER_IP -j dhcpserver
+iptables -t nat -A OUTPUT -p all -d $EXTERNAL_DHCP_SERVER_IP -j dhcpserver
+
+iptables -t nat -A dhcpserver -p all -j DNAT --to-destination $DHCP_SERVER_IP
+
+
+iptables -t nat -I POSTROUTING -p all -s $DHCP_SERVER_IP -j SNAT --to-source $EXTERNAL_DHCP_SERVER_IP
+
+echo 2 > /proc/sys/net/ipv4/conf/eth5/rp_filter
+echo 2 > /proc/sys/net/ipv4/conf/dhcp_server/rp_filter
+
+
+dhcrelay $DHCP_SERVER_IP -m forward -D -d -i eth5
