d82aeff26413e8391f25004cb02924c47de71eab
Sven Dowideit <SvenDowideit@fosiki.com>
2013-11-05 21:28:18 +1000

    it looks like the inverse nat rules make it go - but it needs reboot&test

--- debian-dnsmasq/run_dhcp.sh
+++ debian-dnsmasq/run_dhcp.sh
@@ -18,10 +18,14 @@ export EXTERNAL_DHCP_SERVER_IP=$(ip add show dev dhcp_server | grep 'inet ' | se
 #create the iptables chain
 iptables -t nat -N dhcpserver
 
+#TODO: need to re-write the ipaddress inside the container to add $EXTERNAL_DHCP_SERVER_IP (see the Dockerfile for the wrong one) (maybe re-build every time?)
 /home/sven/src/docker/un/docker-0.6.5-dev run -i -t  -d -name=dnsmasq svendowideit/dnsmasq /bin/bash 
 #dnsmasq -d
 
 export DHCP_SERVER_IP=$(/home/sven/src/docker/un/docker-0.6.5-dev inspect dnsmasq | grep IPAddress | sed 's/",//' | sed 's/.*"//')
+
+
+echo "ROUTING $EXTERNAL_DHCP_SERVER_IP to $DHCP_SERVER_IP"
 
 
 iptables -t nat -A PREROUTING -p all -d $EXTERNAL_DHCP_SERVER_IP -j dhcpserver
@@ -29,11 +33,17 @@ iptables -t nat -A OUTPUT -p all -d $EXTERNAL_DHCP_SERVER_IP -j dhcpserver
 
 iptables -t nat -A dhcpserver -p all -j DNAT --to-destination $DHCP_SERVER_IP
 
+#and the opposite..?
+iptables -t nat -A PREROUTING -p all -d $DHCP_SERVER_IP -j dhcpserver
+iptables -t nat -A OUTPUT -p all -d $DHCP_SERVER_IP -j dhcpserver
+
+iptables -t nat -A dhcpserver -p all -j DNAT --to-destination $EXTERNAL_DHCP_SERVER_IP
+#end opposite
 
 iptables -t nat -I POSTROUTING -p all -s $DHCP_SERVER_IP -j SNAT --to-source $EXTERNAL_DHCP_SERVER_IP
 
 echo 2 > /proc/sys/net/ipv4/conf/eth5/rp_filter
 echo 2 > /proc/sys/net/ipv4/conf/dhcp_server/rp_filter
 
-
+bcrelay -i docker0 -o eth5 &
 dhcrelay $DHCP_SERVER_IP -m forward -D -d -i eth5
