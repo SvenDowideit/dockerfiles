fc20912e280ee41cefb4e9d419eea9f1c8c8319c
Sven Dowideit <SvenDowideit@fosiki.com>
2014-03-24 17:52:42 +1000

    what I'm running atm

--- debian-dnsmasq/run_dhcp.sh
+++ debian-dnsmasq/run_dhcp.sh
@@ -8,10 +8,10 @@ ip link add dhcp_server link eth5 type macvlan mode bridge
 #ip link add ... address 00:11:22:33:44:55
 
 #static ip
-#ip address add 10.10.10.88/24 broadcast 10.10.10.255 dev virtual0
+ip address add 192.168.1.1/24 broadcast 192.168.1.255 dev dhcp_server
 
 #or dynamic
-dhclient dhcp_server
+#dhclient dhcp_server
 
 export EXTERNAL_DHCP_SERVER_IP=$(ip add show dev dhcp_server | grep 'inet ' | sed 's/.*inet //' | sed 's/\/.*//')
 
@@ -19,7 +19,7 @@ export EXTERNAL_DHCP_SERVER_IP=$(ip add show dev dhcp_server | grep 'inet ' | se
 iptables -t nat -N dhcpserver
 
 #TODO: need to re-write the ipaddress inside the container to add $EXTERNAL_DHCP_SERVER_IP (see the Dockerfile for the wrong one) (maybe re-build every time?)
-/home/sven/src/docker/un/docker-0.6.5-dev run -i -t  -d -name=dnsmasq svendowideit/dnsmasq /bin/bash 
+/home/sven/src/docker/un/docker-0.6.5-dev run -i -t -d -name=dnsmasq -p 53:53 -p 67:67 -p 53:53/udp -p 67:67/udp svendowideit/dnsmasq /bin/bash 
 #dnsmasq -d
 
 export DHCP_SERVER_IP=$(/home/sven/src/docker/un/docker-0.6.5-dev inspect dnsmasq | grep IPAddress | sed 's/",//' | sed 's/.*"//')
@@ -34,10 +34,10 @@ iptables -t nat -A OUTPUT -p all -d $EXTERNAL_DHCP_SERVER_IP -j dhcpserver
 iptables -t nat -A dhcpserver -p all -j DNAT --to-destination $DHCP_SERVER_IP
 
 #and the opposite..?
-iptables -t nat -A PREROUTING -p all -d $DHCP_SERVER_IP -j dhcpserver
-iptables -t nat -A OUTPUT -p all -d $DHCP_SERVER_IP -j dhcpserver
+iptables -t nat -A PREROUTING -p all -d $DHCP_SERVER_IP -j eth5
+iptables -t nat -A OUTPUT -p all -d $DHCP_SERVER_IP -j eth5
 
-iptables -t nat -A dhcpserver -p all -j DNAT --to-destination $EXTERNAL_DHCP_SERVER_IP
+iptables -t nat -A eth5 -p all -j DNAT --to-destination $EXTERNAL_DHCP_SERVER_IP
 #end opposite
 
 iptables -t nat -I POSTROUTING -p all -s $DHCP_SERVER_IP -j SNAT --to-source $EXTERNAL_DHCP_SERVER_IP
@@ -47,5 +47,6 @@ echo 2 > /proc/sys/net/ipv4/conf/dhcp_server/rp_filter
 
 
 #I presume some (the dhcrelay?) won't be needed  if i use the docker -p option for port 67
-bcrelay -i docker0 -o eth5 &
-dhcrelay $DHCP_SERVER_IP -m forward -D -d -i eth5
+#bcrelay -i docker0 -o eth5 &
+#dhcrelay $DHCP_SERVER_IP -m forward -D -d -i eth5
+#parprouted -d docker0 eth5
