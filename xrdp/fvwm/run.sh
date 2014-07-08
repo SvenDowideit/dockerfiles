fa79236f32d1da5ada33b52a751b992cd68a48b3
Sven Dowideit <SvenDowideit@docker.com>
2014-07-08 18:44:10 +1000

    Hardcode the dockerx user for now - if you use $UID when su'd, it gets a little more hairy

--- xrdp/fvwm/run.sh
+++ xrdp/fvwm/run.sh
@@ -2,4 +2,4 @@
 
 # TODO: need to choose between --rm and -d
 
-docker run -it --net container:$(hostname) -e DISPLAY -u $UID --volumes-from data -w $(pwd) "$@"
+docker run -it --net container:$(hostname) -e DISPLAY -u dockerx --volumes-from data -w $(pwd) "$@"
