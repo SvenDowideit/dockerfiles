edbffc08aab851461aea87c946c78851e2b3a8cd
Sven Dowideit <SvenDowideit@home.org.au>
2016-01-12 23:05:37 +1000

    Put back the multi-port and multi-link functionality
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- ambassador/run.sh
+++ ambassador/run.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-if [ $1 == "/bin/sh" ]
-then
-    port=$(env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/\1/')
-    target=$(env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/\2:\3/')
-    if [ -z $target ]
-    then
-        echo 'Failed to autodetect target domain and port. Please specify them as arguments.'
-        exit 1
-    fi
-    echo Connecting to $target...
-    echo "socat -t 100000000 TCP4-LISTEN:$port,fork,reuseaddr TCP4:$target & wait" | exec sh
+if [ "$1" == "" ]; then
+	exposedlinks=$(env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/\2:\3/' | sed 'N;s/\n/ /')
+
+	if [ -z "$exposedlinks" ]; then
+		echo 'Failed to autodetect target host/container and port using --link environment. Please specify them as arguments.'
+		exit 1
+	fi
+
+	cmd=$(env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/socat -t 100000000 TCP4-LISTEN:\1,fork,reuseaddr TCP4:\2:\3 \&/')
+	cmd="$cmd wait"
+
+	echo Connecting to $exposedlinks...
+	echo "$cmd" | exec sh
 else
-    echo Connecting to $1:$2...
-    exec socat -t 100000000 TCP4-LISTEN:$2,fork,reuseaddr TCP4:$1:$2
+	echo Connecting to $1:$2...
+	exec socat -t 100000000 TCP4-LISTEN:$2,fork,reuseaddr TCP4:$1:$2
 fi
